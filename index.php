376bbbea2af22eed153dd9254b68a8af951da749
TommasoBernardo <tommasob1210@gmail.com>
2023-05-22 10:28:30 +0200

    created class abstract and implemented 3 function

--- index.php
+++ index.php
@@ -1,8 +1,36 @@
 <?php
-// definizione dell'interfaccia Forma
+// definizione dell'interfaccia 
 interface Vehicle
 {
     // definizione dei metodi
-    public function getName();
-    public function setName($name, $lastname);
+    public function start();
+    public function accelerate();
+    public function stop();
+    public function fuelConsumption();
+}
+
+abstract class Car implements Vehicle
+{
+    private $color = "";
+    private $price = "";
+    private $status = "";
+    private $speed = "";
+
+    // implementazione dei metodi
+    public function start()
+    {
+        // corpo del Metodo
+    }
+
+    // implementazione dei metodi
+    public function accelerate()
+    {
+        // corpo del Metodo
+    }
+
+    // implementazione dei metodi
+    public function stop()
+    {
+        // corpo del Metodo
+    }
 }
